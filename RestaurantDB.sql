4183e5a82ed70d7cf43958470115af0c7f6a877a
Mamta Nath <mamtanathster@gmail.com>
2021-07-28 16:25:56 -0700

    minor tweak in RestaurantDB.sql

--- RestaurantDB.sql
+++ RestaurantDB.sql
@@ -40,7 +40,7 @@ CREATE TABLE orders(
     id int(11) not null auto_increment,
     rest_id int(11) not null,
     customer_id int(11) not null,   -- list of dish Ids
-    phone varchar(16) not null unique,
+    phone varchar(16) not null,
     date Date,
 	pickupOrDeliveryTime varchar(16),
     orderType varchar(20) not null default 'Pickup',  -- Pickup or Delivery
@@ -59,86 +59,87 @@ CREATE TABLE ordered_dishes(
 );
 
 ----------------------------------------------------------------------------
+----------------------------------------------------------------------------
 Insert into restaurants(name, address, restaurantPhone)
-Values("TrueIndianFood", "1 Tulane, Tulane, AZ", "111-111-1111");
+Values('TrueIndianFood', '1 Tulane, Tulane, AZ', '111-111-1111');
 
 Insert into restaurants(name, address, restaurantPhone)
-Values("NumeroUnoFajitas", "2 Julane, FosterCity, CA", "222-111-1111");
+Values('NumeroUnoFajitas', '2 Julane, FosterCity, CA', '222-111-1111');
 
 Insert into restaurants(name, address, restaurantPhone)
-Values("PFChangs", "3 Dulane, FosterCity, PA", "333-111-1111");
+Values('PFChangs', '3 Dulane, FosterCity, PA', '333-111-1111');
 
 -----------------------------------------------------------------------------
 
 Insert into customers(name, phone)
-Values("Sam", "111-222-1111");
+Values('Sam', '111-222-1111');
 
 Insert into customers(name, phone)
-Values("Pam", "111-333-1111");
+Values('Pam', '111-333-1111');
 
 Insert into customers(name, phone)
-Values("Ram", "111-444-1111");
+Values('Ram', '111-444-1111');
 
 -----------------------------------------------------------------------------
 
 Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(1, "Lentil", 6.00, "Vegetarian", "sumptuous savory of Indian spices in soup");
+Values(1, 'Lentil', 6.00, 'Vegetarian', 'sumptuous savory of Indian spices in soup');
 
 Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(1, "Chicken Curry", 7.00, "NonVegetarian", "mouth watering curry made with yogurt, ginger and Indian spices");
+Values(1, 'Chicken Curry', 7.00, 'NonVegetarian', 'mouth watering curry made with yogurt, ginger and Indian spices');
 
 Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(1, "Rice", 4.00, "Vegetarian", "Indian Basmati rice");
+Values(1, 'Rice', 4.00, 'Vegetarian', 'Indian Basmati rice');
 
 Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(1, "Fish", 5.00, "NonVegetarian", "Tasty fish cooked in Indian style");
-
-
-Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(2, "Taco", 6.00, "NonVegetarian", "Yummy taco made with organic ingredients");
-
-Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(2, "Chicken Fajita", 7.00, "NonVegetarian", "Spicy and flavorful authentic mexican fajita");
-
-Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(2, "Rice", 4.00, "Vegetarian", "brown organic delicious rice");
+Values(1, 'Fish', 5.00, 'NonVegetarian', 'Tasty fish cooked in Indian style');
 
 
 Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(3, "Chicken", 9.00, "NonVegetarian", "chicken made with garlic, fresh vegetables in soy sauce");
+Values(2, 'Taco', 6.00, 'NonVegetarian', 'Yummy taco made with organic ingredients');
 
 Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(3, "ChaoMin", 6.00, "Vegetarian", "delicious rice noodle cooked with soy and fresh vegetables");
+Values(2, 'Chicken Fajita', 7.00, 'NonVegetarian', 'Spicy and flavorful authentic mexican fajita');
 
 Insert into dishes(rest_id, dishName, price, typeOfDish, description)
-Values(3, "Eggroll", 7.00, "NonVegetarian", "egg rolled in crispy thin flat dough of rice");
+Values(2, 'Rice', 4.00, 'Vegetarian', 'brown organic delicious rice');
+
+
+Insert into dishes(rest_id, dishName, price, typeOfDish, description)
+Values(3, 'Chicken', 9.00, 'NonVegetarian', 'chicken made with garlic, fresh vegetables in soy sauce');
+
+Insert into dishes(rest_id, dishName, price, typeOfDish, description)
+Values(3, 'ChaoMin', 6.00, 'Vegetarian', 'delicious rice noodle cooked with soy and fresh vegetables');
+
+Insert into dishes(rest_id, dishName, price, typeOfDish, description)
+Values(3, 'Eggroll', 7.00, 'NonVegetarian', 'egg rolled in crispy thin flat dough of rice');
 
 -----------------------------------------------------------------------------
 
 Insert into orders(rest_id, customer_id, phone, date, pickupOrDeliveryTime, orderType, amountDue)
-Values(1, 2, "111-222-1111", "2021-07-20", "10:45", "Pickup", 24.00);
+Values(1, 2, '111-222-1111', '2021-07-20', '10:45', 'Pickup', 24.00);
 
 Insert into orders(rest_id, customer_id, phone, date, pickupOrDeliveryTime, orderType, amountDue)
-Values(2, 3, "111-333-1111", "2021-07-20", "18:30", "Delivery", 26.00);
+Values(2, 3, '111-333-1111', '2021-07-20', '18:30', 'Delivery', 26.00);
 
 -- Insert into orders(rest_id, customer_id, phone, date, pickupOrDeliveryTime, orderType, amountDue)
--- Values(3, 1, "111-444-1111", "2021-07-20",  "19:15", "Delivery", 18.00);
+-- Values(3, 1, '111-444-1111', '2021-07-20',  '19:15', 'Delivery', 18.00);
 ------------------------------------------------------------------------------
 
 Insert into ordered_dishes(order_id, dish_id, quantity)
-Values(3, 1, 1);
+Values(1, 1, 1);
 
 Insert into ordered_dishes(order_id, dish_id, quantity)
-Values(3, 2, 2);
+Values(1, 2, 2);
 
 Insert into ordered_dishes(order_id, dish_id, quantity)
-Values(3, 3, 1);
+Values(1, 3, 1);
 
 Insert into ordered_dishes(order_id, dish_id, quantity)
-Values(4, 5, 2);
+Values(2, 5, 2);
 
 Insert into ordered_dishes(order_id, dish_id, quantity)
-Values(4, 6, 2);
+Values(2, 6, 2);
 
 
 
